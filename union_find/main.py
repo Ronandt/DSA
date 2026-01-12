dd12e8c40233fd1a2551fc07ea8226c60d3afaec
decla <declanisaackuok@gmail.com>
2026-01-12 22:30:19 +0800

    a

--- union_find/main.py
+++ union_find/main.py
@@ -19,13 +19,42 @@ class GraphUnionFind:
         self.array = list(range(0,10))
     def find_value_root(self,a):
         current_val = a
-        while(self.array[current_val] != a):
+        while(self.array[current_val] != current_val):
             current_val = self.array[current_val] 
         return current_val
     def find(self, q, p):
         return self.find_value_root(q) == self.find_value_root(p)
     def union(self, q, p):
         self.array[q] = p
+
+class OptimisedGraphUnionFind:
+    def __init__(self):
+        self.array = list(range(0,10))
+        self.tree_count =[0 for _ in self.array]
+
+    def find_value_root(self,a):
+        current_val = a
+        while(self.array[current_val] != current_val):
+            current_val = self.array[current_val] 
+        return current_val
+    def find(self, q, p):
+        return self.find_value_root(q) == self.find_value_root(p)
+    def union(self, q, p):
+        if(q ==p):
+            return 
+        
+        if(self.tree_count[q] > self.tree_count[p]):
+
+            self.tree_count[p] +=1
+            self.array[p] = q
+        else:
+            self.tree_count[q] +=1
+            self.array[q] =p
+
+            
+#[1,2,3,4,5]
+#[0,0,0,0,0]
+
 
 
                 
